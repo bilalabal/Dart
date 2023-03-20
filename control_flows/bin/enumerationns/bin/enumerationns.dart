2903c488f0521ee147df593cc8984be0a1db8d51
bilal <bilal.webdev@gmail.com>
2023-03-20 10:07:14 +0100

    control flow and enumt

---
+++ control_flows/bin/enumerationns/bin/enumerationns.dart
@@ -0,0 +1,5 @@
+import 'package:enumerationns/enumerationns.dart' as enumerationns;
+
+void main(List<String> arguments) {
+  print('Hello world: ${enumerationns.calculate()}!');
+}
---
+++ enumerations/bin/example5.dart
@@ -0,0 +1,22 @@
+void main(List<String> args) {
+  final vehicle = VehicleType.truck;
+
+  switch (vehicle) {
+    case VehicleType.motorcycle:
+    case VehicleType.bicycle:
+    case VehicleType.car:
+      print('Most common personal vehicles');
+      break;
+
+    case VehicleType.truck:
+      print('Usually used for work');
+      break;
+  }
+}
+
+enum VehicleType {
+  car,
+  truck,
+  motorcycle,
+  bicycle,
+}
